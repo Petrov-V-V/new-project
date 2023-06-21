a8e3ef9391197d02ec9a70b790ce8725cfce4227
Petrov-V-V <vp6514764@gmail.com>
2023-06-21 18:15:02 +0300

    add list of transfers

--- project_03/src/main/java/repositories/Database.java
+++ project_03/src/main/java/repositories/Database.java
@@ -2,13 +2,18 @@ package repositories;
 
 import org.springframework.stereotype.Repository;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  * База данных
  */
 @Repository
 public class Database {
+    ArrayList<ArrayList<Object>> listOfTransfers = new ArrayList<ArrayList<Object>>();
+
     public void writeTransfer(String recipient, BigDecimal amount) {
+        listOfTransfers.add(new ArrayList<Object>(Arrays.asList(recipient, amount)));
         System.out.println("Transfer was written into the database");
     }
 }
