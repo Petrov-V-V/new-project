eafb04f14212b7e219d7896954b0e32e3958c3f5
Petrov-V-V <vp6514764@gmail.com>
2023-07-05 00:01:23 +0300

    add 12th homework

--- project_07/src/components/Cart.js
+++ project_07/src/components/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card, Button, InputNumber, List, Row, Col } from 'antd';
 
 const Cart = ({ cartItems, totalPrice, clearCart, removeFromCart, changeQuantity }) => {
   const handleRemoveFromCart = (item) => {
@@ -15,54 +15,53 @@ const Cart = ({ cartItems, totalPrice, clearCart, removeFromCart, changeQuantity
       window.alert('Корзина пуста!');
     } else {
       window.alert('Оплата произведена!');
+      clearCart();
     }
   };
   
-
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Body> 
-        <Card.Title className="name text-center">Корзина</Card.Title>
-        <div className="list">
-          {cartItems.length === 0 ? (
-            <p className="empty card-text text-center">Пусто</p>
-          ) : (
-            cartItems.map((item) => (
-              <div className="card item" key={item.id}>
-                <div className="card-body">
-                  <Card.Title className="name">{item.name}</Card.Title>
-                  <Card.Text className="price rub">
-                    {item.price} за {item.quantity} шт.
-                  </Card.Text>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div>
-                      <Button variant="danger" onClick={() => handleRemoveFromCart(item)}>Удалить</Button>
+      <Card.Meta
+        title={<div style={{ textAlign: 'center' }}>Корзина</div>}
+        description={
+          <List>
+            {cartItems.length === 0 ? (
+              <p style={{ textAlign: 'center' }}>Пусто</p>
+            ) : (
+              cartItems.map((item) => (
+                <Card key={item.id}>
+                    <div className="name">{item.name}</div>
+                    <div className="price rub">
+                      {item.price} за {item.quantity} шт.
                     </div>
-                    <div>
-                      <Form.Control
-                        type="number"
-                        min="1"
-                        value={item.quantity}
-                        onChange={(e) => handleChangeQuantity(item, e.target.value)}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-        <p className="card-text text-start">
-          <b>Всего: </b>
-          <span className="total-price rub">{totalPrice}</span>
-        </p>
-        <Button variant="primary card-link" onClick={clearCart}>
-          Очистить
-        </Button>
-        <Button variant="primary card-link" onClick={handlePayment}>Оплатить</Button>
-      </Card.Body>
+                    <Row justify="space-between" align="middle">
+                    <Col>
+                        <Button danger onClick={() => handleRemoveFromCart(item)}>
+                          Удалить
+                        </Button>
+                        </Col>
+                        <Col>
+                        <InputNumber
+                          min={1}
+                          value={item.quantity}
+                          onChange={(value) => handleChangeQuantity(item, value)}
+                        />
+                      </Col>
+                    </Row>
+                </Card>
+              ))
+            )}
+          </List>
+        }
+      />
+      <b>Всего: </b>
+      <span className="total-price rub">{totalPrice}</span>
+      <Row justify="space-between" align="middle">
+        <Button onClick={clearCart}>Очистить</Button>
+        <Button onClick={handlePayment}>Оплатить</Button>
+      </Row>
     </Card>
-  );
+    );
 }
 
 export default Cart;
